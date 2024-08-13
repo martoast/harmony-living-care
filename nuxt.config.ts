2cf7ce0aad0e29a83c9861158766376f04252a57
martoast <alexmartos96@gmail.com>
2024-08-13 16:18:51 -0700

    new env and service account json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,12 +17,12 @@ export default defineNuxtConfig({
 
     config: {
       apiKey: process.env.FIREBASE_API_KEY,
-      authDomain: "harmony-livingcare.firebaseapp.com",
-      projectId: "harmony-livingcare",
-      storageBucket: "harmony-livingcare.appspot.com",
-      messagingSenderId: "755371459745",
-      appId: "1:755371459745:web:85242ba9227859a6c01ce0",
-      measurementId: "G-7BQQVFYDLG"
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
+      measurementId: process.env.FIREBASE_MEASUREMENT_ID
     },
   },
   nitro: {
@@ -36,7 +36,9 @@ export default defineNuxtConfig({
     },
   },
   runtimeConfig: {
+    GOOGLE_APPLICATION_CREDENTIALS: process.env.GOOGLE_APPLICATION_CREDENTIALS,
     public: {
+      GOOGLE_APPLICATION_CREDENTIALS: process.env.GOOGLE_APPLICATION_CREDENTIALS,
       MAPBOX_API_TOKEN: process.env.MAPBOX_API_TOKEN,
     }
   }
