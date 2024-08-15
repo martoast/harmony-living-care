a4fd273c0ab99e7e056f38a6edca1295a45d17d0
martoast <alexmartos96@gmail.com>
2024-08-15 12:58:19 -0700

    navbar fix

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -49,7 +49,7 @@
             </div>
           </div>
           <div class="flex items-center">
-            <div class="flex-shrink-0">
+            <div v-if="!route.path.includes('communities')" class="flex-shrink-0">
               <a href="/login">
                 <button type="button" class="relative inline-flex items-center gap-x-1.5 rounded-md bg-red-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-500">
                   <PlusIcon class="-ml-0.5 h-5 w-5" aria-hidden="true" />
@@ -130,6 +130,9 @@
   const auth = useFirebaseAuth()
 
   const router = useRouter()
+
+  const route = useRoute()
+
 
   const isActiveRoute = (route) => {
     const currentPath = router.currentRoute.value.path;
--- pages/communities/[id].vue
+++ pages/communities/[id].vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-white px-6 py-32 lg:px-8">
+    <div class="bg-white px-6 pt-12 lg:pt-32 pb-32 lg:px-8">
       <div class="mx-auto max-w-3xl text-base leading-7 text-gray-700">
         <p class="text-base font-semibold leading-7 text-red-500">Assisted Living Property</p>
         <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{{ property.address }}</h1>
